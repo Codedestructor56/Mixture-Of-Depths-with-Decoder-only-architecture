80436f33214f95319627466afcb6c9aca60aa411
EterG <richardfeynman438@gmail.com>
2024-05-03 19:22:37 +0500

    Initial commit

---
+++ transformer.py
@@ -0,0 +1,130 @@
+from utils import *
+import numpy as np
+
+class Encoder(nn.Module):
+    def __init__(self, params: ModelParams):
+        super().__init__()
+        self.device = params.device
+        self.emb_dim = params.emb_dim
+        self.thresh = params.thresh
+        self.norm = RMSnorm(self.emb_dim, self.device, self.thresh)
+        self.attention = GQattention(params)
+        self.ffn = SwiGLu_Forward(params)
+
+    def forward(self, x:torch.Tensor, cur_pos: Optional[int])->torch.Tensor:
+        first_layer = x + self.attention(self.norm(x),cur_pos) 
+        second_layer = first_layer + self.ffn(self.norm(first_layer))
+
+        del first_layer
+        torch.cuda.empty_cache()
+        return second_layer
+
+
+class MODRouter(nn.Module):
+    def __init__(self, params: ModelParams):
+        super().__init__()
+        self.k_tokens = params.k_tokens if params.k_tokens is not None else params.max_seq_len
+        assert self.k_tokens<=params.max_seq_len, "k should be lower"
+        self.device = params.device
+        self.n_layers = params.n_layers
+        self.seq_len = params.max_seq_len
+        self.emb_dim = params.emb_dim
+        self.r_weight_layers = nn.ModuleList([nn.Linear(self.emb_dim, 1) for i in range(self.n_layers)]).to(self.device)
+        self.encoders = nn.ModuleList([Encoder(params) for i in range(self.n_layers)]).to(self.device)
+                   
+    def forward(self, x:torch.tensor)->torch.Tensor:
+        res = x.to(self.device)
+        for layer in range(self.n_layers):
+            route_weights = self.r_weight_layers[layer](res)
+            flattened_weights = route_weights.squeeze(2)
+            topk_indices = torch.topk(flattened_weights, k=self.k_tokens).indices
+            sorted_top_k = torch.sort(topk_indices, dim=-1).values           
+            chosen_tokens = res[torch.arange(res.size(0)).unsqueeze(1),sorted_top_k]
+            
+            all_indices = torch.arange(flattened_weights.shape[1]).repeat(flattened_weights.shape[0],1).to(self.device)
+           
+            #please find out a way to compute the following bs in pytorch
+            np_all_indices = all_indices.to("cpu").numpy()
+            np_sorted_top_k = sorted_top_k.to("cpu").numpy()
+            fin_arr = []
+            for idx in range(all_indices.shape[0]):
+                diff = np.setdiff1d(np_all_indices[idx], np_sorted_top_k[idx])
+                fin_arr.append(diff)
+
+            botk_indices = torch.tensor(fin_arr, dtype = torch.int32).to(self.device)
+            rejected_tokens = res[torch.arange(res.size(0)).unsqueeze(1),botk_indices]
+            
+            res = self.encoders[layer](chosen_tokens,None)
+
+            res = res+rejected_tokens
+
+            del route_weights,flattened_weights,topk_indices,sorted_top_k,chosen_tokens,all_indices,np_sorted_top_k,np_all_indices,botk_indices,rejected_tokens
+            torch.cuda.empty_cache()
+        return res
+
+
+class Transformer(nn.Module):
+    def __init__(self, params: ModelParams):
+        super().__init__()
+        self.device = params.device
+        self.vocab_size = params.vocab_size
+        self.emb_dim = params.emb_dim
+        self.thresh = params.thresh
+        self.norm = RMSnorm(self.emb_dim, self.device, self.thresh)  
+        self.div_batch = params.div_batch
+        self.linear = nn.Linear(params.emb_dim, self.vocab_size).to(self.device)
+        self.embeddings = nn.Embedding(self.vocab_size, self.emb_dim).to(self.device)
+        self.mod = MODRouter(params)
+
+    def forward(self, x:torch.Tensor, cur_pos: Optional[int])->torch.Tensor:
+        assert self.div_batch<=x.shape[0], "Batch serializer should not exceed tensor dimensions"
+        res = self.embeddings(x)
+        #layer_count = 0
+        #stats = None
+        #try:
+        res = self.norm(self.mod(res))
+        # layer_count+=1
+           #stats = torch.cuda.memory_stats()
+         #I'm only serializing the following linear layer because I'm poor af(PLEASE GIB A100)
+        res = torch.chunk(res, self.div_batch, dim = 0)
+       
+        accumulated_output = None
+        for chunk_idx in range(len(res)):
+            out = self.linear(res[chunk_idx])
+            if accumulated_output is None:
+                accumulated_output = out
+            else:
+                # Concatenate the current output with the accumulated output along the specified dimension
+                accumulated_output = torch.cat((accumulated_output, out), dim=0)
+    
+            del out
+            torch.cuda.empty_cache()
+
+        #print(f"Final Chunk shape: {np.array(res_chunks).shape}")
+       
+        probas = nn.functional.softmax(accumulated_output, dim = 1)
+        del accumulated_output, res
+        torch.cuda.empty_cache()
+        return probas 
+        #except:
+        #   print(f"Stopped at {layer_count}")
+        #    print(f"Memory stats for your convenience: {stats}")
+        #    del res
+        #   torch.cuda.empty_cache()
+        #    return torch;.zeros(x.shape[0], self.vocab_size)
+
+ds = load_dataset("HuggingFaceTB/cosmopedia-100k", split="train")
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B")
+data = ModelParams(emb_dim=512, use_cache = False, device = device, num_heads=16, kv_num_heads=None,
+                   max_batch_size=10, max_seq_len=256, ffn_hidden_dim=512, theta=None, thresh=None,
+                   n_layers = 1, vocab_size=tokenizer.vocab_size+1, div_batch=10, k_tokens = 128)
+
+
+dataloader = DataLoader(Data(ds, tokenizer, device, data.max_seq_len), batch_size = data.max_batch_size, collate_fn=Data(ds, tokenizer, device, data.max_seq_len).collate_fn)
+print(f"Vocab Size: {tokenizer.vocab_size}")
+to_load = next(iter(dataloader)).to(device)
+transformer = Transformer(data)
+print(transformer(to_load, None))
+
---
+++ utils.py
@@ -0,0 +1,187 @@
+import torch
+import numpy as np
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from sentencepiece import SentencePieceProcessor
+from transformers import AutoTokenizer
+from torch.nn.utils.rnn import pad_sequence
+from datasets import load_dataset
+import sentencepiece as spm
+from typing import Optional
+from dataclasses import dataclass
+import math
+import os
+
+
+@dataclass    
+class ModelParams:
+    emb_dim: int 
+    use_cache: bool
+    kv_num_heads: Optional[int]
+    num_heads: int
+    device: str
+    max_seq_len: int
+    max_batch_size: int
+    ffn_hidden_dim: int
+    theta: Optional[int]
+    thresh:Optional[float]
+    n_layers: int
+    vocab_size: int
+    div_batch: int
+    k_tokens: Optional[int]
+
+def train_tokenizer(vocab_size: int, dataset, model_prefix:str="tokenizer", model_type: str="bpe"):
+    concatenated_texts = []
+    for idx in range(len(dataset)):
+        prompt = dataset['prompt'][idx]
+        text = dataset['text'][idx]
+        concatenated_text = f"Question: {prompt} Answer: {text}"
+        concatenated_texts.append(concatenated_text)
+
+    with open(f"{model_prefix}.txt", "w", encoding="utf-8") as f:
+        for text in concatenated_texts:
+            f.write(text + "\n")
+    spm.SentencePieceTrainer.Train(
+        f'--input={model_prefix}.txt --model_prefix={model_prefix} --vocab_size={vocab_size} --model_type={model_type}')
+   
+
+class Data(Dataset):
+    def __init__(self, dataset, tokenizer, device:str, max_seq_len:Optional[int]):
+        self.dataset = dataset
+        self.tokenizer = tokenizer
+        self.max_seq_len = max_seq_len
+        self.device = device
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        prompt =  self.dataset['prompt'][idx]
+        text = self.dataset['text'][idx]
+        concat_text = f"Question: {prompt} Answer: {text}"
+        tokenized_text = self.tokenizer.encode(concat_text)
+       
+        to_ret = torch.tensor(tokenized_text, dtype = torch.int32).to(self.device)
+        return to_ret[:self.max_seq_len]
+
+    def collate_fn(self, batch):
+        padded_batch = pad_sequence(batch, batch_first=True, padding_value=0)
+        return padded_batch.to(self.device)
+
+class RotaryEmbeddings(nn.Module):
+    def __init__(self, device:str, theta: int =10000):
+        super().__init__()
+        self.theta = theta
+        self.device = device
+
+    def forward(self, x: torch.Tensor, seq_len:Optional[int]=None, emb_dim:Optional[int]=None)->torch.Tensor:
+        batch_size, seq_len, emb_dim = x.shape
+        assert emb_dim%2==0, "Embeddings dimension must be even"
+        #Q_i=10000^(-2(i-1)/emb_dim)
+        thetas = (1.0/self.theta**((2*torch.arange(0,emb_dim,2))//emb_dim)).to(self.device)
+        thetas_repeated = thetas.unsqueeze(0).repeat(seq_len, 1)
+        thetas_true = thetas_repeated * (torch.arange(seq_len, device = self.device)+1).unsqueeze(1)
+        #calculate the rotation matrices using these thetas, apply them on the embeddings in  2D or complex space
+        matrix_rot = torch.stack((torch.sin(thetas_true),torch.cos(thetas_true)),dim=-1).to(self.device)
+        comp_matrix = torch.view_as_complex(matrix_rot).unsqueeze(0)
+        x_reshaped = torch.view_as_complex(x.reshape(batch_size, seq_len, emb_dim//2, 2))
+        rotated_x = torch.view_as_real(x_reshaped * comp_matrix).squeeze(-1).reshape(batch_size, seq_len, emb_dim).to(self.device)
+        del x_reshaped, comp_matrix, matrix_rot, thetas_true, thetas_repeated, thetas
+        torch.cuda.empty_cache()
+        return rotated_x
+
+
+class GQattention(nn.Module):
+    def __init__(self, params: ModelParams):
+        super().__init__()
+        self.use_cache = params.use_cache
+        self.device = params.device
+        self.pos_rotor = RotaryEmbeddings(self.device)
+
+        self.num_heads = params.num_heads
+        self.kv_num_heads = params.kv_num_heads if params.kv_num_heads is not None else params.num_heads
+        assert params.emb_dim % self.num_heads==0, "Make the embedding dim divisible by num_heads"
+        self.head_dim = params.emb_dim//self.num_heads
+        self.kv_head_dim = params.emb_dim//self.kv_num_heads if self.kv_num_heads is not None else self.head_dim 
+
+        self.wq = nn.Linear(params.emb_dim, self.num_heads*self.head_dim).to(self.device)
+        self.wk = nn.Linear(params.emb_dim, self.kv_num_heads*self.kv_head_dim).to(self.device)
+        self.wv = nn.Linear(params.emb_dim, self.kv_num_heads*self.kv_head_dim).to(self.device)
+        self.wo = nn.Linear(params.emb_dim, self.num_heads*self.head_dim).to(self.device)
+        if self.use_cache:
+            self.c_v = torch.zeros((params.max_batch_size, params.max_seq_len, self.kv_num_heads, self.kv_head_dim))
+            self.c_k = torch.zeros((params.max_batch_size, params.max_seq_len, self.kv_num_heads, self.kv_head_dim))
+
+    def forward(self, x:torch.Tensor, cur_pos: Optional[int]=None)->torch.Tensor:
+        batch_size, seq_len, emb_dim = x.shape
+        query = self.wq(x)
+        key = self.wk(x)
+        value = self.wv(x)
+        output = self.wo(x)
+        
+        xq = self.pos_rotor(query).reshape(batch_size, seq_len, self.num_heads, self.head_dim)
+        xv = self.pos_rotor(value).reshape(batch_size, seq_len, self.kv_num_heads, self.kv_head_dim)
+        xk = key.reshape(batch_size, seq_len, self.kv_num_heads, self.kv_head_dim)
+        
+        if self.use_cache:
+            self.c_v[:batch_size, cur_pos:cur_pos+seq_len]=xv
+            self.c_k[:batch_size, cur_pos:cur_pos+seq_len]=xk
+            
+            keys = self.c_k[:batch_size, :cur_pos+seq_len]
+            values = self.c_v[:batch_size, :cur_pos+seq_len]
+
+            n_rep = self.num_heads//self.kv_num_heads
+
+            keys = keys[:,:,:,None,:].expand(keys.shape[0], keys.shape[1],
+                                           self.kv_num_heads, n_rep, self.kv_head_dim).reshape(keys.shape[0],
+                                            keys.shape[1], self.kv_num_heads*n_rep, self.kv_head_dim)
+
+            values = values[:,:,:,None,:].expand(values.shape[0], values.shape[1],
+                                                 self.kv_num_heads, n_rep, self.kv_head_dim).reshape(values.shape[0],
+                                                 values.shape[1], self.kv_num_heads*n_rep, self.kv_head_dim)
+
+        else:
+            keys = xq
+            values = xv
+        
+        xq = xq.permute(0, 2, 1, 3).contiguous().to(self.device)
+        keys = keys.permute(0, 2, 3, 1).contiguous().to(self.device)
+        values = values.permute(0, 2, 1, 3).contiguous().to(self.device)
+        
+        query_key_score = torch.matmul(xq, keys)/math.sqrt(self.head_dim)
+        attention_score = torch.matmul(query_key_score, values).transpose(1,2).contiguous().reshape(batch_size, seq_len, -1)
+        output = self.wo(attention_score)
+
+        del query_key_score, attention_score, xq, keys, values 
+        torch.cuda.empty_cache()
+        #make sure that the dimensions are correct and that the training and inferencing parts are compatible
+        return output
+
+class RMSnorm(nn.Module):
+    def __init__(self, dim:int, device:str, thresh: float = 1e-4):
+        super().__init__()
+        self.params = nn.Parameter(torch.ones(dim))
+        self.thresh = thresh
+        self.device = device
+
+    def forward(self, x:torch.Tensor)->torch.Tensor:
+        denom = torch.sqrt(x.pow(2).mean(-1,keepdims=True)).to(self.device)
+        res = ((x.to(self.device))*self.params.to(self.device))/denom
+        del denom
+        torch.cuda.empty_cache()
+        return res
+
+class SwiGLu_Forward(nn.Module):
+    def __init__(self, params:ModelParams):
+        super().__init__()
+        self.hidden_dim = params.ffn_hidden_dim
+        self.device = params.device
+        self.w1 = nn.Linear(params.emb_dim, self.hidden_dim).to(self.device)
+        self.w2 = nn.Linear(params.emb_dim, self.hidden_dim).to(self.device)
+        self.w3 = nn.Linear(self.hidden_dim, params.emb_dim).to(self.device)
+
+    def forward(self, x:torch.Tensor)->torch.Tensor:
+        return self.w3(self.w2(x)*nn.functional.silu(self.w1(x)))
+
+
+
